0b7773f6ce37c19be12a5832351c66ab2f4817db
pbd-kn <75879364+pbd-kn@users.noreply.github.com>
2021-10-29 19:08:07 +0200

    Update config.php

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -1,6 +1,5 @@
 <?php
 
-array_insert($GLOBALS['TL_CTE'], 2, array('besslichschmuck' => array('gallery_creator_ce_news' => 'Markocupic\GalleryCreatorBundle\ContentGalleryCreatorNews')));
 array_insert($GLOBALS['TL_CTE'], 2, array('besslichschmuck' => array('schmuckartikel' => 'PBDKN\ContaoBesslichschmuck\Resources\contao\classes\elements\ContentSchmuckartikel')));
 
 
